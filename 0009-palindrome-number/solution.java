1d8cab39694e89720e325fb92739615eed5ca1e9
Maaz N. <maaz73209@gmail.com>
2026-01-14 19:04:57 +0000

    Sync LeetCode submission - Palindrome Number - Runtime - 5 ms (79.32%), Memory - 46 MB (33.79%)

---
+++ 0009-palindrome-number/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+            if(x<0||(x!=0&&x%10==0))return false;
+        int y=0;
+        while(x>y){
+            y=(y*10)+x%10;
+            x=x/10;
+        }
+        return y==x ||x==y/10;
+
+        
+    }
+}
